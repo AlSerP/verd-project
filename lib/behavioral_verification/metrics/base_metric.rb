235eaa45c8db981181ba7a9c11cdd6e05bb1c6f7
AlSerP <gosha1416@yandex.ru>
2024-04-15 11:59:37 +0300

    Delete old files | Update Base::Metric class

--- lib/behavioral_verification/metrics/base_metric.rb
+++ lib/behavioral_verification/metrics/base_metric.rb
@@ -1,63 +1,65 @@
-module BehavioralVerification::Metrics
-  class BaseMetric
-    def initialize(user, logging=true)
-      @user = user
-      @logging = logging
-    end
+module BehavioralVerification
+  module Metrics
+    # Base verification metric class
+    class BaseMetric
+      def initialize(user, logging: true)
+        @user = user
+        @logging = logging
+      end
 
-    def update(data)
-      bm = Benchmark.measure do
-        begin
-          no_log_update(data)
-        rescue Exception => e
-          exception = e
+      # Updates user data with
+      # new behavioral data
+      def update(data)
+        bm = Benchmark.measure do
+          begin
+            no_log_update(data)
+          rescue Exception => e
+            exception = e
+          end
+        end
+
+        if exception
+          log(message: "Metric data has been updated", time: bm)
+        else
+          log_exception(exception: exception)
         end
       end
-      
-      if exception
-        log(message: 'Updated metric data', time: bm)
-      else
-        log_exception(exception: exception)
-      end
 
-      log "Update result is #{@pairs}"
-    end
-
-    def verificate(data)
-      # Проверяет вероятность ввода пароля владельцем
-      # и возвращает уровень уверенности от 0 (не владелец) до 2 (владелец)
-  
-      bm = Benchmark.measure do
-        begin
-          trustness = no_log_verificate(data)
-        rescue Exception => e
-          exception = e
+      # Сalculates the level of confidence in the user
+      # using current metric
+      def verificate(data)
+        bm = Benchmark.measure do
+          begin
+            trustness = no_log_verificate(data)
+          rescue Exception => e
+            exception = e
+          end
         end
+
+        if exception
+          log(verification_result: trustness, time: bm)
+        else
+          log_exception(exception: exception)
+        end
+
+        trustness
       end
-      
-      if exception
-        log(verification_result: trustness, time: bm)
-      else
-        log_exception(exception: exception)
+
+      private
+
+      def no_log_verificate(data); end
+      def no_log_update(data); end
+
+      def log(params)
+        return unless @logging
+
+        MyLogger.logger.info params.merge({ class: self.class.to_s })
       end
 
-      trustness
-    end
+      def log_exception(params)
+        return unless @logging
 
-    private
-
-    def no_log_verificate(data); end
-    def no_log_update(data); end
-
-    def log(params)
-      if @logging
-        MyLogger.logger.info params.merge({ class: 'Metrics::KeyboardFP'})
-      end
-    end
-
-    def log_exception(params)
-      if @logging
-        MyLogger.logger.error params.merge({ class: 'Metrics::KeyboardFP' })
+        MyLogger.logger.error params.merge({ class: self.class.to_s })
       end
     end
   end
